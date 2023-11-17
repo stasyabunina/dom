4a3d619e542019f43e5f3e05d953ff94de75e37d
Anastasia <tryes9856@gmail.com>
2023-11-17 06:49:52 +0300

    add files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Домашнее задание к занятию "2. DOM"
+![CI](https://github.com/stasyabunina/dom/actions/workflows/web.yml/badge.svg)
---
+++ src/js/GameArea.js
@@ -0,0 +1,24 @@
+export default class GameArea {
+  constructor(element) {
+    this._element = element;
+  }
+
+  goblinMoves() {
+    const goblinImg = document.createElement('img');
+    goblinImg.classList.add('area__img');
+    goblinImg.src = 'https://github.com/netology-code/ahj-homeworks/raw/AHJ-50/dom/pic/goblin.png';
+    goblinImg.alt = 'Гоблин';
+
+    const area = document.querySelectorAll('.area__item');
+
+    for (const cell of area) {
+      if (cell.firstElementChild) {
+        cell.firstElementChild.remove();
+      }
+    }
+
+    const randomCell = Math.floor(Math.random() * area.length);
+
+    area[randomCell].append(goblinImg);
+  }
+}
